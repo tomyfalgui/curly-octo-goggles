958e1613dc2ef234c80039a4efc6cd4eb388825e
Tomy Falgui <tomyfalgui@gmail.com>
2022-08-31 05:01:43 +0800

    create model function for creating and retrieving comments

---
+++ server/models.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  },
+})
+
+function retrieveComments() {
+  return knex('comments').select('*')
+}
+
+function createComment({ name, content }) {
+  return knex('comments').insert({ name, content, upvotes: 0 })
+}
+
+module.exports = {
+  retrieveComments,
+  createComment,
+}
