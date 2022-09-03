6a927f7a9c989f0fd57b45d46b2bda619ba06a2c
Tomy Falgui <tomyfalgui@gmail.com>
2022-09-04 01:01:07 +0800

    install and setup websockets

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const http = require('http')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+
+const { Server } = require('socket.io')
 
 const { retrieveComments, createComment, upvoteComment } = require('./models')
 
 const app = express()
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+})
 
 app.use(cors('*'))
 app.use(bodyParser.json())
@@ -45,6 +54,13 @@ app.put('/comments/:id/upvote', async (req, res) => {
   }
 })
 
-app.listen(4444, function () {
+io.on('connection', socket => {
+  socket.on('upvote', id => {
+    console.log('upvote', id)
+    socket.broadcast.emit('upvote', id)
+  })
+})
+
+server.listen(4444, function () {
   console.log('Listening on PORT 4444')
 })
