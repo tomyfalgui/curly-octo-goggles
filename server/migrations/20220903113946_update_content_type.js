6eeccf419dfe0339fe71dc22fa13373958d0e659
Tomy Falgui <tomyfalgui@gmail.com>
2022-09-03 21:02:21 +0800

    change content type from string to text

---
+++ server/migrations/20220903113946_update_content_type.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('comments', function (table) {
+    table.text('content').notNullable().alter()
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('comments', function (table) {
+    table.string('content').notNullable().alter()
+  })
+}
