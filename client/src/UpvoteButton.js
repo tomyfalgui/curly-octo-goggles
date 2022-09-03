931e3f8735933f6aafcee368b291beff85da7146
Tomy Falgui <tomyfalgui@gmail.com>
2022-09-04 01:01:29 +0800

    add realtime updates to upvotes

--- client/src/UpvoteButton.js
+++ client/src/UpvoteButton.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { io } from 'socket.io-client'
 import { BACKEND } from './main.js'
 
 export default function UpvoteButton({ id, upvotes: fetchedUpvotes }) {
   const [upvotes, setUpvotes] = React.useState(+fetchedUpvotes)
+  const [socket, setSocket] = React.useState()
+
+  React.useEffect(() => {
+    const socket = io(BACKEND)
+    socket.on('upvote', function (receivedId) {
+      if (receivedId == id) {
+        setUpvotes(upvote => upvote + 1)
+      }
+    })
+    setSocket(socket)
+
+    return () => {
+      socket.disconnect()
+    }
+  }, [])
 
   function upvoteComment() {
     fetch(`${BACKEND}/comments/${id}/upvote`, {
@@ -15,12 +31,13 @@ export default function UpvoteButton({ id, upvotes: fetchedUpvotes }) {
       .then(resp => {
         if (resp.ok) {
           setUpvotes(upvote => upvote + 1)
+          socket.emit('upvote', id)
           return
         }
         throw new Error('upvote failed')
       })
       .catch(() => {
-        setUpvotes(upvote => upvote - 1)
+        console.error('upvote failed')
       })
   }
 
